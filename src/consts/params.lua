6a0fb13601ab91ccd83fedb6fe1b276ab1718f70
5uperb0y <fanjenyan@gmail.com>
2023-08-29 22:06:21 +0800

    feat: count and display message cards
    wy-10

---
+++ src/consts/params.lua
@@ -0,0 +1,48 @@
+local params = {}
+local colors = require("src.consts.colors")
+-- TODO: lua seems not supports using a table value as other table's key. Thus, I create temporary variables to store those colors.
+params.messageZoneParameters = {
+	[colors.white]= {
+		messageDeck = {type = "ScriptingTrigger", position = {1.31, 3.84, -14.33}, scale = {1, 5.1, 1} },
+		blackMessageCountDisplay = {type = "3DText", position = {-2.85, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		redMessageCountDisplay = {type = "3DText", position = {-5.48, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end  },
+		blueMessageCountDisplay = {type = "3DText", position = {-4.15, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end}
+	},
+	[colors.red] = {
+		messageDeck = {type = "ScriptingTrigger", position = {-13.71, 3.84, -14.33},  scale = {1, 5.1, 1} },
+		blackMessageCountDisplay = {type = "3DText", position = {-17.84, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		blueMessageCountDisplay = {type = "3DText", position = {-19.17, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		redMessageCountDisplay = {type = "3DText", position = {-20.5, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end}
+	},
+	[colors.pink]   = {
+		messageDeck = {type = "ScriptingTrigger", position = {16.35, 3.84, -14.33},  scale = {1, 5.1, 1} },
+		blackMessageCountDisplay = {type = "3DText", position = {12.17, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		redMessageCountDisplay = {type = "3DText", position = {9.51, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		blueMessageCountDisplay = {type = "3DText", position = {10.84, 1.35, -8.15}, scale = {1, 1, 1}, rotation = {90, 0, 0}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end}
+	},
+	[colors.yellow] = {
+		messageDeck = {type = "ScriptingTrigger", position = {-19.36, 3.84, -0.95},  scale = {1, 5.1, 1} },
+		blackMessageCountDisplay = {type = "3DText", position = {-13.2, 1.35, 3.13}, scale = {1, 1, 1}, rotation = {90, 0, -90}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		blueMessageCountDisplay = {type = "3DText", position = {-13.2, 1.35, 4.46}, scale = {1, 1, 1}, rotation = {90, 0, -90}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		redMessageCountDisplay = {type = "3DText", position = {-13.2, 1.35, 5.79}, scale = {1, 1, 1}, rotation = {90, 0, -90}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end}
+	},
+	[colors.green]  = {
+		messageDeck = {type = "ScriptingTrigger", position = {-16.18, 3.84, 15.34},  scale = {1, 5.1, 1} },
+		blackMessageCountDisplay = {type = "3DText", position = {-12.21, 1.35, 9.23}, scale = {1, 1, 1}, rotation = {90, 0, 180}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		blueMessageCountDisplay = {type = "3DText", position = {-10.88, 1.35, 9.23}, scale = {1, 1, 1}, rotation = {90, 0, 180}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		redMessageCountDisplay = {type = "3DText", position = {-9.55, 1.35, 9.23}, scale = {1, 1, 1}, rotation = {90, 0, 180}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end}
+	},
+	[colors.blue]   = {
+		messageDeck = {type = "ScriptingTrigger", position = {13.71, 3.84, 15.34},    scale = {1, 5.1, 1} },
+		blackMessageCountDisplay = {type = "3DText", position = {17.81, 1.35, 9.23}, scale = {1, 1, 1}, rotation = {90, 0, 180}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		blueMessageCountDisplay = {type = "3DText", position = {19.14, 1.35, 9.23}, scale = {1, 1, 1}, rotation = {90, 0, 180}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		redMessageCountDisplay = {type = "3DText", position = {20.47, 1.35, 9.23}, scale = {1, 1, 1}, rotation = {90, 0, 180}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end}
+	},
+	[colors.purple] = {
+		messageDeck = {type = "ScriptingTrigger", position = {19.47, 3.84, 1.81},    scale = {1, 5.1, 1} },
+		blackMessageCountDisplay = {type = "3DText", position = {13.2, 1.35, -2.34}, scale = {1, 1, 1}, rotation = {90, 0, 90}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		blueMessageCountDisplay = {type = "3DText", position = {13.2, 1.35, -3.67}, scale = {1, 1, 1}, rotation = {90, 0, 90}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end},
+		redMessageCountDisplay = {type = "3DText", position = {13.2, 1.35, -5}, scale = {1, 1, 1}, rotation = {90, 0, 90}, callback_function = function(spawned_object) spawned_object.TextTool.setValue("0") end}
+	}
+}
+return params
--- src/index.lua
+++ src/index.lua
@@ -9,6 +9,41 @@ local utils = require("src.utils.utils")
 require("src.config")
 local setup = require("src.setup")
 local start = require("src.start")
+-- test
+local params = require("src.consts.params")
+local messageZoneParameters = params.messageZoneParameters
+local messageZoneMeta = {}
+local metadata = require("src.consts.metadata")
+local actCardMeta = metadata.actCard
+local count_msg = require("src.count_msg")
+for player, zones in pairs(messageZoneParameters) do
+	if utils.contains(getSeatedPlayers(), player) then
+		for type, params in pairs(zones) do
+			local obj = spawnObject(params)
+			messageZoneMeta[obj.getGUID()] = {["owner"] = player, ["type"] = type}
+			messageZoneParameters[player][type]["zone"] = obj
+		end
+	end
+end
+function onObjectEnterZone(zone, object)
+	if messageZoneMeta[zone.getGUID()] and messageZoneMeta[zone.getGUID()]["type"] == "messageDeck" then
+		local messageFreq = count_msg.countMsg(actCardMeta, count_msg.getCardGUIDsfromZone(zone))
+		local player = messageZoneMeta[zone.getGUID()]["owner"]
+		messageZoneParameters[player]["blackMessageCountDisplay"].zone.setValue(tostring(messageFreq.black or 0))
+		messageZoneParameters[player]["redMessageCountDisplay"].zone.setValue(tostring((messageFreq.red or 0) + (messageFreq.dual or 0)))
+		messageZoneParameters[player]["blueMessageCountDisplay"].zone.setValue(tostring((messageFreq.blue or 0) + (messageFreq.dual or 0)))
+	end
+end
+function onObjectLeaveZone(zone, object)
+	if messageZoneMeta[zone.getGUID()] and messageZoneMeta[zone.getGUID()]["type"] == "messageDeck" then
+		local messageFreq = count_msg.countMsg(actCardMeta, count_msg.getCardGUIDsfromZone(zone))
+		local player = messageZoneMeta[zone.getGUID()]["owner"]
+		messageZoneParameters[player]["blackMessageCountDisplay"].zone.setValue(tostring(messageFreq.black or 0))
+		messageZoneParameters[player]["redMessageCountDisplay"].zone.setValue(tostring((messageFreq.red or 0) + (messageFreq.dual or 0)))
+		messageZoneParameters[player]["blueMessageCountDisplay"].zone.setValue(tostring((messageFreq.blue or 0) + (messageFreq.dual or 0)))
+	end
+
+end
 -- project onLoad
 function onLoad()
     log.dev('onLoad')
